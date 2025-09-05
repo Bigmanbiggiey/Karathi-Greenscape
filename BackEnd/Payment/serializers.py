7b9ea3a216c941cdf27afc58c01c6b0cc579cfb9
Bigmanbiggiey <ndiranguh02@gmail.com>
2025-09-05 20:45:44 +0300

    Update serializers.py

--- BackEnd/Payment/serializers.py
+++ BackEnd/Payment/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from .models import Payment
+from Shop.serializers import OrderSerializer
 
 class PaymentSerializer(serializers.ModelSerializer):
+    order = OrderSerializer(read_only=True)
+    
     class Meta:
         model = Payment
         fields = "__all__"
-        read_only_fields = ["status", "mpesa_receipt_number", "transaction_date"]
+        read_only_fields = ["status", "mpesa_receipt_number", "transaction_date", "raw_response"]
