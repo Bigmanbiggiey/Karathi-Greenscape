9aa80cbef22ed29eb7d33b413814c1614f699645
Bigmanbiggiey <ndiranguh02@gmail.com>
2025-09-10 09:31:28 +0300

    Updates

--- BackEnd/BackEnd/settings.py
+++ BackEnd/BackEnd/settings.py
@@ -158,6 +158,8 @@ SIMPLE_JWT = {
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ("Bearer",),
+    "USER_ID_FIELD": "user_id",      # match your primary key
+    "USER_ID_CLAIM": "user_id",
 }
 
 AUTH_USER_MODEL = "Auth.CustomUser"
