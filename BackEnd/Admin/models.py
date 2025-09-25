18cfb6a35bbb7b9361e804d1fc51603473bb6f06
Bigmanbiggiey <ndiranguh02@gmail.com>
2025-09-25 22:32:59 +0300

    .

--- BackEnd/Admin/models.py
+++ BackEnd/Admin/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class admin_keys(models.Model):
+    name = models.CharField(max_length=20)
+    key = models.CharField(max_length=10 unique=true)
--- BackEnd/Auth/models.py
+++ BackEnd/Auth/models.py
@@ -2,17 +2,20 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
+    username = models.CharField(max_length=30)
+
     email = models.EmailField(unique=True)
     user_type = models.CharField(
         max_length=20,
-        choices=[("customer", "Customer"), ("admin", "Admin"), ("vendor", "Vendor")],
+        choices=[("customer", "Customer"), ("staff", "Staff"), ("admin", "Admin")],
         default="customer",
     )
     user_id = models.AutoField(primary_key=True)
-    billing_address = models.TextField(blank=True, null=True)
+    billing_address = models.TextField(blank=True, null=False)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
 
-    REQUIRED_FIELDS = ["email"]
-    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return f"{self.username} ({self.user_type})"
