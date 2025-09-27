5b7f19412ee72fef0a72e5260f47e907d610e434
Bigmanbiggiey <ndiranguh02@gmail.com>
2025-09-27 13:14:21 +0300

    Update models.py

--- BackEnd/Admin/models.py
+++ BackEnd/Admin/models.py
@@ -2,4 +2,6 @@ from django.db import models
 
 class admin_keys(models.Model):
     name = models.CharField(max_length=20)
-    key = models.CharField(max_length=10 unique=true)
+    key = models.CharField(
+        max_length=10 unique=true
+    )
